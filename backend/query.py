64b105455471867763a8c7b23738b91852f09449
Coroush <45938250+Coroush@users.noreply.github.com>
2024-04-20 09:40:47 -0600

    Update query.py

--- backend/query.py
+++ backend/query.py
@@ -1,15 +1,19 @@
 import os
 from llama_index.core import StorageContext, load_index_from_storage
 
-print("loading storage")
-storage_context = StorageContext.from_defaults(persist_dir="store")
-index = load_index_from_storage(storage_context)
+def init():
+    print("loading storage")
+    storage_context = StorageContext.from_defaults(persist_dir="store")
+    index = load_index_from_storage(storage_context)
 
-query_engine = index.as_query_engine()
+    query_engine = index.as_query_engine()
+    
+    return query_engine
 
-while True:
-    query = input("Enter a query: ")
-    if query == "exit":
-        break
+def ask(query_engine, query):
+    # query = input("Enter a query: ")
+    # if query == "exit":
+    #     break
     response = query_engine.query(query)
     print(response)
+    return response
