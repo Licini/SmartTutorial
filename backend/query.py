5fa9ee64a1f1446b955e204f710bd312113aac79
fordj <mil01@free.fr>
2024-04-20 16:30:03 +0000

    makefile frontend

--- backend/query.py
+++ backend/query.py
@@ -3,7 +3,7 @@ from llama_index.core import StorageContext, load_index_from_storage
 
 def init():
     print("loading storage")
-    storage_context = StorageContext.from_defaults(persist_dir="store")
+    storage_context = StorageContext.from_defaults(persist_dir="../store")
     index = load_index_from_storage(storage_context)
 
     query_engine = index.as_query_engine()
