02e6002f298454810fee312e1f810303e7b5ff63
Coroush <45938250+Coroush@users.noreply.github.com>
2024-04-20 09:41:31 -0600

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -2,11 +2,12 @@ import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
-
+from query import init, ask
 
 app = FastAPI()
 origins = ["*"]
 
+query_engine = init()
 
 class Message(BaseModel):
     message: str
@@ -23,9 +24,12 @@ app.add_middleware(
 
 @app.post("/")
 async def echo_message(message: Message):
+    global query_engine
+    
+    message.message = ask(query_engine, message.message)
+
     responses = [
         {"from": "chatGpt", "data": message.message, "format": "text"},
-        {"from": "chatGpt", "data": message.message, "format": "code"},
+        # {"from": "chatGpt", "data": message.message, "format": "code"},
     ]
     return responses
-
